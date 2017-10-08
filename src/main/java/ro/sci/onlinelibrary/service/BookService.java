d576991f32ca8a6f2825e86136855ee6808bc1e1
Ursu Ivett <ursuivett@gmail.com>
2017-10-08 21:42:23 +0300

    bookview

--- src/main/java/ro/sci/onlinelibrary/service/BookService.java
+++ src/main/java/ro/sci/onlinelibrary/service/BookService.java
@@ -5,13 +5,18 @@ import ro.sci.onlinelibrary.model.book.Book;
 import java.util.List;
 
 /**
- * Created by iulia on 9/15/2017.
+ * Created by ivett on 9/15/2017.
  */
 public interface BookService<T extends Book> {
 
+    void add(T t);
+    void update(T t);
+    void delete(T t);
     List<T> findAll ();
 
     List<T> findBookByAuthor (String author);
+
+    List<T> findBookByTitle (String title);
 
     List<T> findByField (String field);
 
@@ -19,6 +24,5 @@ public interface BookService<T extends Book> {
 
     List<T> findBookByAuthorAndPublishingHouse (String author, String publishingHouse);
 
-    void add(T t);
-    void update(T t);
+    T searchById(Integer bookId);
 }
