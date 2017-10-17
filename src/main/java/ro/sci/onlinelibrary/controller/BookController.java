52f8f8e00753c0b2f4575c3517dd4fe785fb2865
Dragan Tudor <tudordragan27@gmail.com>
2017-10-17 03:04:43 +0300

    kk

--- src/main/java/ro/sci/onlinelibrary/controller/BookController.java
+++ src/main/java/ro/sci/onlinelibrary/controller/BookController.java
@@ -1,26 +1,24 @@
 package ro.sci.onlinelibrary.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ro.sci.onlinelibrary.model.book.Book;
 import ro.sci.onlinelibrary.repository.BookRepository;
 import ro.sci.onlinelibrary.service.BookService;
-
 import java.util.List;
 
-/**
- * Created by iulia on 9/18/2017.
- */
-@RestController
+@Controller
 public class BookController {
 
     @Autowired
     private BookService bookService;
+    @Autowired
     private BookRepository bookRepository;
 
+    //Show all books
     @RequestMapping(value = "/books", method = RequestMethod.GET)
     @ResponseBody
     public ModelAndView getBooks() {
@@ -28,6 +26,7 @@ public class BookController {
         return new ModelAndView("booksView", "books", books);
     }
 
+    //Search books from repository
     @RequestMapping(value = "/books/search", method = RequestMethod.GET)
     @ResponseBody
     public ModelAndView searchBooks(@RequestParam(value = "searchField", required = false, defaultValue = "") String search) {
@@ -45,7 +44,7 @@ public class BookController {
     //Ask submit new book
     @GetMapping(value = "/newBook")
     public String bookForm(Model model) {
-        model.addAttribute("book",new Book());
+        model.addAttribute("book", new Book());
         return "submit";
     }
 
