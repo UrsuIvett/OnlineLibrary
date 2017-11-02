eb777b16e2ec56aeae4b6dfca6ed09915ab9cc0a
UrsuIvett <ursuivett@gmail.com>
2017-11-02 22:54:08 +0200

    Add files via upload

---
+++ mylibrary.sql
@@ -0,0 +1,4 @@
+﻿SELECT *from books
+
+INSERT INto books (id,title,author,publishinghouse,booktype,booklanguage,nrpages,isbn)
+values(26,'Sub Dom-Vol2','Stephen King','Nemira','DRAMA','ENGLISH',345,'1051')
